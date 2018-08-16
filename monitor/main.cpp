60c3775dca6ab5c7b013d590fb12723aa1b10f25
Simon Stürz <simon.stuerz@guh.io>
2018-08-16 20:57:35 +0200

    Add license headers

--- monitor/main.cpp
+++ monitor/main.cpp
@@ -1,3 +1,24 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                               *
+ * Copyright (C) 2018 Simon Stürz <simon.stuerz@guh.io>                          *
+ *                                                                               *
+ * This file is part of nymea-remoteproxy.                                       *
+ *                                                                               *
+ * This program is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by          *
+ * the Free Software Foundation, either version 3 of the License, or             *
+ * (at your option) any later version.                                           *
+ *                                                                               *
+ * This program is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 *
+ * GNU General Public License for more details.                                  *
+ *                                                                               *
+ * You should have received a copy of the GNU General Public License             *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.         *
+ *                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include <QUrl>
 #include <QCoreApplication>
 #include <QLoggingCategory>
@@ -23,8 +44,6 @@ int main(int argc, char *argv[])
                                      .arg(SERVER_VERSION_STRING)
                                      .arg(API_VERSION_STRING)
                                      .arg(QChar(0xA9)));
-
-
 
 
     QCommandLineOption tokenOption(QStringList() << "s" << "socket", "The AWS token for authentication. Default /tmp/", "socket");
--- tests/test-offline/nymea-remoteproxy-tests-offline.h
+++ tests/test-offline/nymea-remoteproxy-tests-offline.h
@@ -1,14 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                               *
+ * Copyright (C) 2018 Simon Stürz <simon.stuerz@guh.io>                          *
+ *                                                                               *
+ * This file is part of guh-cloudproxy.                                          *
+ *                                                                               *
+ * This program is free software: you can redistribute it and/or modify          *
+ * it under the terms of the GNU General Public License as published by          *
+ * the Free Software Foundation, either version 3 of the License, or             *
+ * (at your option) any later version.                                           *
+ *                                                                               *
+ * This program is distributed in the hope that it will be useful,               *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 *
+ * GNU General Public License for more details.                                  *
+ *                                                                               *
+ * You should have received a copy of the GNU General Public License             *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.         *
+ *                                                                               *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef NYMEA_REMOTEPROXY_TESTS_OFFLINE_H
 #define NYMEA_REMOTEPROXY_TESTS_OFFLINE_H
-
-#include <QUrl>
-#include <QtTest>
-#include <QSslKey>
-#include <QObject>
-#include <QWebSocket>
-#include <QHostAddress>
-#include <QSslCertificate>
-#include <QSslConfiguration>
 
 #include "basetest.h"
 
@@ -20,6 +32,7 @@ class RemoteProxyOfflineTests : public BaseTest
     Q_OBJECT
 public:
     explicit RemoteProxyOfflineTests(QObject *parent = nullptr);
+    ~RemoteProxyOfflineTests() = default;
 
 private slots:
     // Basic stuff
